a3bc4dc8bec27f6c5108a4805ef4030fc1951d24
MyskUnion <50514582+MyskUnion@users.noreply.github.com>
2019-08-16 11:33:11 +0300

    Add files via upload

---
+++ NewClass1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Пекарня
+ */
+public class NewClass1 {
+
+    /**
+     *
+     * @param args
+     */
+ 
+        
+        // int x = 100500;
+        // long y = 4543658768979089L;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Name:")
+        System.out.println("Surname:");
+        System.out.println("Years old:");
+        System.out.println("Sex:");
+        System.out.println("Password:");
+        
+        
+        
+        
+       
+    }
+    
+}
